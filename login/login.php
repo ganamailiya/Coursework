b17b4fd7af6884f373b4daa9e46d4df3c6a2678c
iliyaganama <1510537@rgu.ac.uk>
2016-03-18 20:23:49 +0000

    bug info

--- login/login.php
+++ login/login.php
@@ -35,7 +35,7 @@
 			//Otherwise echo error.
 			
 			if(mysqli_num_rows($result) == 1) {
-				header("location: home.php"); // Redirecting To Other Page
+				header("url = home.php"); // Redirecting To Other Page
 
 
 			}
