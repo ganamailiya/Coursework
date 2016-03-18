f4758f68ba96cfb047286ccf856702018d7b3ca7
iliyaganama <1510537@rgu.ac.uk>
2016-03-18 20:41:51 +0000

    bug info

---
+++ Bug.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iliyaganama
+ * Date: 18/03/2016
+ * Time: 8:35 PM
+ */
+include ("connection.php");
+$msg = "";
+
+if(isset($_POST["submit"])) {
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $mobile = $_POST["mobile"];
+    $name = mysqli_real_escape_string($db, $name);
+    $email = mysqli_real_escape_string($db, $email);
+    $password = mysqli_real_escape_string($db, $password);
+    $password = md5($password);
+    $mobile = mysqli_real_escape_string($db, $mobile);
+
+
+    $sql="SELECT email FROM users WHERE email='$email'";
+    $result=mysqli_query($db,$sql);
+    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+    if(mysqli_num_rows($result) == 1)
+    {
+        $msg = "Sorry...This email already exists...";
+    }
+    else
+    {
+        //echo $name." ".$email." ".$password;
+        $query = mysqli_query($db, "INSERT INTO users (username, email, password, phone)VALUES ('$name', '$email', '$password', '$mobile')")
+        or die(mysqli_error($db));
+        if($query)
+        {
+            $msg = "Thank You! you are now registered.";
+            echo $msg;
+        }
+
+    }
+}
+?>
+
