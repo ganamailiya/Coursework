1512e7348f2d49993c3918cbb10dccd035ffc3e2
iliyaganama <1510537@rgu.ac.uk>
2016-03-22 22:30:17 +0000

    getting the Buglist

---
+++ buglist.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iliyaganama
+ * Date: 22/03/2016
+ * Time: 9:32 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+<img class="img1" src="images/%20logo.png ">
+    <div class="links">
+    <a href="index.html">Home Page</a> &nbsp;&nbsp;
+    <a href="login.html">Login</a> &nbsp;&nbsp;
+    <a href="signup.html">Signup</a>
+    </div>
+
+<h1>Bug List</h1>
+<div id="content">
+    <?php
+    //Establish connection with DB
+    include ("connection.php");
+
+    //select all from bugs table
+    $sql = "select * from bugs";
+
+    //fetch result from DB
+    $result = mysqli_query($db,$sql);
+
+    //we scan each row
+    while($row = mysqli_fetch_assoc($result)){
+        //get bug title and id
+        $bugtitle = $row['title'];
+        $bugID = $row['bugID'];
+
+        //write link to page
+        echo '<a href="bugss.php?id="'.$bugID.'>'.$bugtitle.'</a></br>';
+    }
+    ?>
+</div>
+</body>
+</html>
---
+++ bugss.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iliyaganama
+ * Date: 22/03/2016
+ * Time: 9:33 PM
+ */
+?>
+
+<body>
+<div id="content">
+    <?php
+    // est DB connect
+    include ("login/login.php");
+
+    $sql = "select * from bugs WHERE bugID = ".$_GET["bid"];
+
+    $result = mysqli_query($db,$sql);
+
+    $row = mysqli_fetch_assoc($result);
+
+    $bugtitle = $row['title'];
+    $bugID = $row['bugID'];
+    $bugdesc = $row['description'];
+
+    echo "<h2>".$bugtitle."</h2>";
+    echo "<p>".$bugtitle."</p>";
+
+    ?>
+</div>
+</body>
