6d6ad7e47a5fc70bc6da21f197a4b53330bc36f2
iliyaganama <1510537@rgu.ac.uk>
2016-03-25 13:35:12 +0000

    getting the Buglist

--- bugss.php
+++ bugss.php
@@ -25,53 +25,29 @@
     echo $url;
 
     ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Bug Comments</title>
-    <link type="text/css" rel="stylesheet" href="style.css"/>
-</head>
-<body>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Bug Comments</title>
+        <link type="text/css" rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
 
-<img class="img1" src="images/%20logo.png ">
-<div class="links">
-    <a href="login/home.php">Home Page</a> &nbsp;&nbsp;
-    <a href="login/logout.php">Logout</a> &nbsp;&nbsp;
-</div>
-<br><br>
-<nav class = "format">
-    <?php
-    echo "<h2>" . "Bug Title: " .$bugtitle."</h2>";
-    echo "<p>" . "Bug Description: " .$bugdesc."</p>";
-    ?>
-    <h3>Add Comment</h3>
-    <form method="GET" action="comment.php">
-        <table cellpadding="10" align="center" id = "signup">
-            <tbody>
-            <tr>
-                <td>
-                    Comment
-                    <br>
-                    <br>
-                    <br>
-                </td>
-                <td>
-                    <textarea cols="30" rows="4" name="comment" style="width: 330px; height: 88px;"></textarea>
-                </td>
-            </tr>
-            <tr>
-                <td align="center" colspan="2">
-                    <input type="submit" value="Add Comment" name="submit">
-
-                </td>
-            </tr>
-            </tbody>
-        </table>
-    </form>
-</nav>
-<footer>
-    <p>(c) 2016 Ganama's Design</p>
-</footer>
-</body>
-</html>
+    <img class="img1" src="images/%20logo.png ">
+    <div class="links">
+        <a href="login/home.php">Home Page</a> &nbsp;&nbsp;
+        <a href="login/logout.php">Logout</a> &nbsp;&nbsp;
+    </div>
+    <br><br>
+    <nav class = "format">
+        <?php
+        echo "<h2>" . "Bug Title: " .$bugtitle."</h2>";
+        echo "<p>" . "Bug Description: " .$bugdesc."</p>";
+        ?>
+    </nav>
+    <footer>
+        <p>(c) 2016 Ganama's Design</p>
+    </footer>
+    </body>
+    </html>
--- comment.php
+++ comment.php
@@ -30,7 +30,7 @@ $bid = $bugID;
 echo $comment;
 echo $username;
 
-if(isset($_GET["submit"])) {
+if(isset($_POST["submit"])) {
 
     $query = mysqli_query($db, "INSERT INTO comments (comment, postDate, userID, bugID) VALUES ('$comment', now(), '$userID', '$bid')")
     or die(mysqli_error($db));
@@ -40,3 +40,53 @@ else{
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bug Comments</title>
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+<img class="img1" src="images/%20logo.png ">
+<div class="links">
+    <a href="login/home.php">Home Page</a> &nbsp;&nbsp;
+    <a href="login/logout.php">Logout</a> &nbsp;&nbsp;
+</div>
+<br><br>
+<nav class = "format">
+    <?php
+    echo "<h2>" . "Bug Title: " .$bugtitle."</h2>";
+    echo "<p>" . "Bug Description: " .$bugdesc."</p>";
+    ?>
+    <h3>Add Comment</h3>
+    <form method="POST" action="comment.php">
+        <table cellpadding="10" align="center" id = "signup">
+            <tbody>
+            <tr>
+                <td>
+                    Comment
+                    <br>
+                    <br>
+                    <br>
+                </td>
+                <td>
+                    <textarea cols="30" rows="4" name="comment" style="width: 330px; height: 88px;"></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" colspan="2">
+                    <input type="submit" value="Add Comment" name="submit">
+
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
+</nav>
+<footer>
+    <p>(c) 2016 Ganama's Design</p>
+</footer>
+</body>
+</html>
