ba3a1e2250d30b337904ab7a604b447b81a3c06c
iliyaganama <1510537@rgu.ac.uk>
2016-03-25 20:51:25 +0000

    getting the Buglist

--- login/home.php
+++ login/home.php
@@ -38,6 +38,7 @@ $username= $_SESSION['username'];
 	<a href="../buglist.php"> <input type = "button" value = "BUG LIST" ></a> &nbsp;&nbsp;
 </div>
 <br><br><br>
-<footer> </footer>
+<footer id = "footer">
+Designed By Iliya Ganama 2016 </footer>
 </body>
 </html>
